3dc18bd9c50320582422f43da1782a2f9f1289e3
Ryan Yurkanin <yurkaninryan@hotmail.com>
2014-11-16 13:46:15 -0500

    new

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     path = require('path'),
     lucid = express();
 
@@ -6,8 +7,14 @@ lucid.set('port', process.env.PORT);
 
 lucid.use(express.static('lucid'));
 
+lucid.use(bodyParser());
+
 lucid.get('/', function(req, res) {
     res.status(200).sendFile(path.join(__dirname, 'lucid', 'dist', 'pages', 'index.html'));
+});
+
+lucid.get('/essayresponse', function(req, res) {
+    res.status(200).sendFile(path.join(__dirname, 'lucid', 'dist', 'pages', 'essayresponse.html'));
 });
 
 lucid.get('/admin', function(req, res) {
@@ -18,14 +25,42 @@ lucid.get('/*', function(req, res) {
     res.status(200).sendFile(path.join(__dirname, 'lucid', 'dist', 'pages', '404.html'));
 });
 
+lucid.post('/essayresponse', function(req, res) {
+    var essay = req.body.essay;
+    var txt = essay.split(" ");
+    var arraylen = txt.length;
+    var log = false;
+    var syn;
+    for(i = 0; i<arraylen; i++){
+        if(txt[i] == "very" || txt[i] == "Very"){
+            txt[i] = "";
+            log = true;
+        }
+        else if(log == true){
+            syn = thesaurus.find(txt[i]);
+            txt[i] = syn[i%5];
+            log = false;
+        }if(txt[i] == "get"){
+            syn = thesaurus.find("get");
+            txt[i] = syn[i%5];
+        }
+    }
+    console.log(txt);
+    var last = '';
+    for(j=0; j<arraylen; j++){
+        if(txt[j] != ""){
+            last += txt[j] + ' ';
+        }
+    }
+    console.log(last);
+    console.log("This essay is " + arraylen +" words long");
+});
+
+
+
 lucid.listen(lucid.get('port'), function() {
     console.log('Express server listening on port %d in %s mode', lucid.get('port'), lucid.get('env'));
 });
 
-lucid.post('/essayresponse');
-console.log("works");
-
 var thesaurus = require("thesaurus");
 var updated_thesaurus = thesaurus.load("./th_en_US_new.dat");
-
-console.log(thesaurus.find("sad"));
